f38bfbd98ad2b27778482ee69319e4bb0640f3a2
superpoussin22 <vincent.nadal@orange.fr>
2018-01-17 18:23:30 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 cat <<EOF
-Welcome to NGINX reverse proxy for MobSF
+Welcome to the marvambass/nginx-ssl-secure container
+
+IMPORTANT:
+  IF you use SSL inside your personal NGINX-config,
+  you should add the Strict-Transport-Security header like:
+
+    # only this domain
+    add_header Strict-Transport-Security "max-age=31536000";
+    # apply also on subdomains
+    add_header Strict-Transport-Security "max-age=31536000; includeSubdomains";
+
+  to your config.
+  After this you should gain a A+ Grade on the Qualys SSL Test
+
 EOF
 
 if [ -z ${DH_SIZE+x} ]
 then
-  >&2 echo ">> no \$DH_SIZE specified using default"
+  >&2 echo ">> no \$DH_SIZE specified using default" 
   DH_SIZE="2048"
 fi
 
 
-DH="/etc/nginx/external/pki/dh.pem"
+DH="/etc/nginx/pki/dh.pem"
 
 if [ ! -e "$DH" ]
 then
@@ -23,22 +36,20 @@ then
   openssl dhparam -out "$DH" $DH_SIZE
 fi
 
-if [ ! -e "/etc/nginx/external/pki/cert.pem" ] || [ ! -e "/etc/nginx/external/pki/key.pem" ]
+if [ ! -e "/etc/nginx/pki/cert.pem" ] || [ ! -e "/etc/nginx/pki/key.pem" ]
 then
   echo ">> generating self signed cert"
   openssl req -x509 -newkey rsa:4086 \
   -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=localhost" \
-  -keyout "/etc/nginx/external/pki/key.pem" \
-  -out "/etc/nginx/external/pki/cert.pem" \
+  -keyout "/etc/nginx/pki/key.pem" \
+  -out "/etc/nginx/pki/cert.pem" \
   -days 3650 -nodes -sha256
 fi
 
-echo ">> copy /etc/nginx/external/*.conf files to /etc/nginx/conf.d/"
-cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null
 envsubst < /etc/nginx/conf.d/auth.conf > /etc/nginx/conf.d/auth2.conf
 rm -f /etc/nginx/conf.d/auth.conf
 mv /etc/nginx/conf.d/auth2.conf /etc/nginx/conf.d/auth.conf
-cp /etc/nginx/external/auth.htpasswd /etc/nginx/auth.htpasswd
+#cp /etc/nginx/external/auth.htpasswd /etc/nginx/auth.htpasswd
 
 # exec CMD
 echo ">> exec docker CMD"
